5c67b0e80952309ccaa34fb9b25c04104c92c726
lucascidade <lucascidade96@gmail.com>
2023-11-07 09:32:50 -0400

    refinando arquivo de config da API

--- Configuration.cs
+++ Configuration.cs
@@ -9,9 +9,9 @@ public static class Configuration
 
     public class SmtpConfiguration
     {
-        public string Host { get; set; }
+        public string Host { get; set; } = string.Empty;
         public int Port { get; set; } = 25;
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
